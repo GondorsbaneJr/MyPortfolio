e43b753c51fd1c84f676db0d95f6b61592967756
GondorsbaneJr <wprickett545@gmail.com>
2023-03-15 00:10:52 -0400

    s

---
+++ contact.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Contact</title>
+</head>
+    <body>
+        <!-- Navigation -->
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+        <h1>Contact</h1>
+        <p>Phone: 0435684596</p>
+        <p>Email: <a href="mailto:wpkeleher@williamkeleher.tech"> </p>
+        <p>Located in Brisbane, Australia</p>
+        <p>I'm not available for hire at this time</p>
+        <p>Thank you for your interest</p>
+    </body>
+</html>
